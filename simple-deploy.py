6cd4bdddb24405153df723ff033266f0565bbc80
Vicent <vicentgodella@gmail.com>
2016-02-25 23:12:11 +0100

    Add environment

--- simple-deploy.py
+++ simple-deploy.py
@@ -7,6 +7,7 @@ tmp_directory = '/tmp/_simple-deploy/' + str(os.getpid())
 os.makedirs(tmp_directory)
 cache_directory = tmp_directory + '/cache'
 
+
 def deploy(url):
     filename = tmp_directory + '/file.tar.gz'
     os.system('/usr/local/bin/aws s3 cp ' + url + ' ' + filename)
@@ -15,20 +16,22 @@ def deploy(url):
     os.system('tar xfz ' + filename + ' -C ' + cache_directory)
 
 
-def install():
+def install(environment):
     os.chdir(cache_directory)
-    os.system('./install.sh ')
+    os.system('./install.sh {}'.format(environment))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Deploy a package.')
     parser.add_argument('url', help='S3 url of the package to install: s3://BUCKET/KEY.tar.gz')
+    parser.add_argument('environment', help='Environment', choices=["pre", "prod"])
 
     args = parser.parse_args()
     url = args.url
+    environment = args.environment
 
     deploy(url)
-    install()
+    install(environment)
     # Clean up
     os.system('rm -rf ' + tmp_directory)
 
